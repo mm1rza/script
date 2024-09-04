68b62bd73de3254216488af6dbc9877b9be02d71
MRJA <74845201+mm1rza@users.noreply.github.com>
2024-09-04 09:43:48 +0700

    v1

--- mm1rza.ps1
+++ mm1rza.ps1
@@ -2,7 +2,8 @@
 Write-Host "Pilih salah satu opsi berikut:"
 Write-Host "1. Aktivasi Windows dan Office"
 Write-Host "2. Install Spotify Anti-Iklan"
-Write-Host "3. Download dan Ekstrak File mm1rza ke C:\#mirza"
+Write-Host "3. Download dan Ekstrak File mm1rza ke C:\#mirza (PowerShell Windows)"
+Write-Host "4. Download dan Ekstrak File mm1rza ke C:\#mirza (PowerShell 7)"
 $choice = Read-Host "Masukkan pilihan Anda (1/2/3):"
 
 switch ($choice) {
@@ -18,6 +19,10 @@ switch ($choice) {
     "3" {
         # Download dan Ekstrak File mm1rza ke C:\#mirza
 		Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"iex (iwr 'https://mnet.my.id/update/github/c' -UseBasicParsing)`"" -Verb RunAs
+    }
+	"4" {
+        # Download dan Ekstrak File mm1rza ke C:\#mirza
+		iex (iwr "https://mnet.my.id/update/github/c" -UseBasicParsing -AllowInsecureRedirect)
     }
     default {
         Write-Host "Pilihan tidak valid. Silakan coba lagi."
